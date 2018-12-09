ed907a7d3c8e3d723b32f4c6361ef25f7a0995e3
Sean Jun <seanjun21@gmail.com>
2018-12-09 01:36:33 -0700

    Understanding State in React Components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // We called setState!!!!
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      },
+      /* Reason why the Loading div displays indefinitely is
+        because the request to the geolocation api hangs on indefinitely on the back-end, giving an error code 3.
+        A partial fix is adding a third argument to the getCurrentPosition call '{ timeout: 10000 }'
+        which returns a timeout error after 10 seconds of the request being made. */
+      { timeout: 10000 }
+    );
+  }
+
+  // React says we need to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
