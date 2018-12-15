d10d123bffd43f36097edb1c981ad0241dc40f4a
Sean Jun <seanjun21@gmail.com>
2018-12-14 17:52:44 -0700

    season project done

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   constructor(props) {
@@ -7,33 +9,34 @@ class App extends React.Component {
 
     // THIS IS THE ONLY TIME we do direct assignment to this.state
     this.state = { lat: null, errorMessage: '' };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // We called setState!!!!
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      },
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
       /* Reason why the Loading div displays indefinitely is
         because the request to the geolocation api hangs on indefinitely on the back-end, giving an error code 3.
         A partial fix is adding a third argument to the getCurrentPosition call '{ timeout: 10000 }'
         which returns a timeout error after 10 seconds of the request being made. */
-      { timeout: 10000 }
+      // { timeout: 10000 }
     );
   }
 
-  // React says we need to define render!!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  // React says we need to define render!!
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
