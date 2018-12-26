b7ffb94009f81632bc61af98f228c47519addfb2
Sean Jun <sjun@digitalairstrike.com>
2018-12-25 19:58:56 -0700

    Making API Requests with React

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
-require('dotenv').config();
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(process.env);
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: {
-        query: term
-      },
-      headers: {
-        Authorization: `Client-ID ${process.env.YOUR_ACCESS_KEY}`
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await unsplash.get(
+      'https://api.unsplash.com/search/photos',
+      {
+        params: { query: term }
       }
-    });
-  }
+    );
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
