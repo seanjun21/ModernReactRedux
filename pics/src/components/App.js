ad9acbe453c9296128ca76c2f40ea1506ca5898c
Sean Jun <seanjun21@gmail.com>
2018-12-25 17:52:52 -0700

    setting env variable for pics

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+require('dotenv').config();
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    console.log(process.env);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: `Client-ID ${process.env.YOUR_ACCESS_KEY}`
+      }
+    });
   }
 
   render() {
